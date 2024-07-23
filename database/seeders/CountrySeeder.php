a4bd655480b04ca63b68b6b2dacc05c7b7ae17c3
David Garay <garaekz@gmail.com>
2024-07-22 18:28:26 -0700

    Add seeders

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $countries = [
+            ['code' => 'US', 'name' => 'United States'],
+            ['code' => 'CA', 'name' => 'Canada'],
+            ['code' => 'MX', 'name' => 'Mexico'],
+        ];
+
+        foreach ($countries as $country) {
+            Country::create($country);
+        }
+    }
+}
---
+++ database/seeders/UserCommentSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use App\Models\Country;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UserCommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->count(30)
+            ->countries(Country::pluck('id')->toArray())
+            ->create()
+            ->each(function (User $user) {
+                $this->createCommentsForUser($user);
+            });
+    }
+
+    private function createCommentsForUser(User $user): void
+    {
+        $start = now()->subDays(7);
+        $end = now();
+
+        $dates = $this->generateDateRange($start, $end);
+
+        foreach ($dates as $date) {
+            Comment::factory()->count(rand(1, 10))->create([
+                'created_at' => $date,
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+
+    private function generateDateRange(Carbon $start, Carbon $end): array
+    {
+        $dates = [];
+
+        for ($date = $start; $date->lte($end); $date->addDay()) {
+            $dates[] = $date;
+        }
+
+        return $dates;
+    }
+}
