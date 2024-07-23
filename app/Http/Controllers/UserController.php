80bf65f8e9f6e35289c99686030e5f5d5ff3e6a9
David Garay <garaekz@gmail.com>
2024-07-22 22:37:54 -0700

    Filters on request and query scopes to fetch correct data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserTableRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(UserTableRequest $request)
     {
-        $users = User::paginate(request('per_page', 5));
+        $query = User::query();
 
-        return UserResource::collection($users);
+        if ($request->has('filter.country')) {
+            $query->countryCode($request->input('filter.country'));
+        }
+
+        if ($request->has('filter.comment_activity_trend')) {
+            $query->byTrend($request->input('filter.comment_activity_trend'));
+        }
+
+        if ($request->has('sort')) {
+            $query->customSort($request->input('sort.field'), $request->input('sort.direction'));
+        }
+
+        return UserResource::collection($query->paginate(request('per_page', 5)));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -30,5 +31,61 @@ class User extends Model
     public function getFullNameAttribute(): string
     {
         return "{$this->first_name} {$this->last_name}";
+    }
+
+    public function scopeCountryCode($query, string $code): void
+    {
+        $query->whereHas('country', function ($query) use ($code) {
+            $query->where('code', $code);
+        });
+    }
+
+    public function scopeByTrend($query, string $trend)
+    {
+        $yesterday = Carbon::yesterday()->toDateString();
+        $today = Carbon::today()->toDateString();
+
+        $subQueryYesterday = Comment::selectRaw('COUNT(*)')
+            ->whereColumn('comments.user_id', 'users.id')
+            ->whereDate('comments.created_at', $yesterday);
+
+        $subQueryToday = Comment::selectRaw('COUNT(*)')
+            ->whereColumn('comments.user_id', 'users.id')
+            ->whereDate('comments.created_at', $today);
+
+        $query->addSelect([
+            'yesterday_count' => $subQueryYesterday,
+            'today_count' => $subQueryToday,
+        ]);
+
+        return match ($trend) {
+            'higher' => $query->havingRaw('today_count > yesterday_count'),
+            'lower' => $query->havingRaw('today_count < yesterday_count'),
+            'neutral' => $query->havingRaw('today_count = yesterday_count'),
+            default => $query,
+        };
+    }
+
+    public function scopeCustomSort($query, string $field, string $direction): void
+    {
+        if ($field === 'comments_count') {
+            $query->withCount(['comments' => function ($query) {
+                $query->whereDate('created_at', today());
+            }])->orderBy('comments_count', $direction);
+        } else {
+            $query->orderBy($field, $direction);
+        }
+    }
+
+    public function getCommentsTodayCountAttribute(): int
+    {
+        return $this->comments->where('created_at', '>=', now()->startOfDay())->count();
+    }
+
+    public function getCommentsYesterdayCountAttribute(): int
+    {
+        return $this->comments->where('created_at', '>=', now()->subDay()->startOfDay())
+            ->where('created_at', '<', now()->startOfDay())
+            ->count();
     }
 }
