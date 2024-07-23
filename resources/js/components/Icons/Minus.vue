3ef7a33eebc13a4ebedba34ad8ed57864446a162
David Garay <garaekz@gmail.com>
2024-07-22 22:35:29 -0700

    Add svg icons as components

---
+++ resources/js/components/Icons/Minus.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="size-5">
+        <svg
+            data-slot="icon"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+        >
+            <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z"
+            ></path>
+        </svg>
+    </span>
+</template>
